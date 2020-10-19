ead80bd3bf541db934fc7b06936adf573b90f569
ZahiraBarriento <zbarriento@gmail.com>
2020-10-19 18:12:44 -0300

    firebase

--- LaComanda/src/environments/environment.ts
+++ LaComanda/src/environments/environment.ts
@@ -6,6 +6,17 @@ export const environment = {
   production: false
 };
 
+export var firebaseConfig = {
+  apiKey: "AIzaSyCO2B6OBXfSUpH_CWOl9Jzj1by3XKurtZI",
+  authDomain: "comservy-e3aa9.firebaseapp.com",
+  databaseURL: "https://comservy-e3aa9.firebaseio.com",
+  projectId: "comservy-e3aa9",
+  storageBucket: "comservy-e3aa9.appspot.com",
+  messagingSenderId: "648289619522",
+  appId: "1:648289619522:web:c7642d629ceff2101d67db",
+  measurementId: "G-54JHZE9ZGQ"
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
