cf4024ef0306dbd2900adfafef6cf1e1f52c059c
Juan Carlos Bugallo <juancarlosjaviermdq@gmail.com>
2020-11-20 19:33:28 -0300

    update

--- LaComanda/src/app/services/qr.service.ts
+++ LaComanda/src/app/services/qr.service.ts
@@ -12,7 +12,7 @@ export class QrService implements OnInit {
   pudoIngresarAlLocal = false;
   qrMesas: any = [];
   private options: BarcodeScannerOptions = {
-    formats: "PDF_417,QR_CODE"
+    formats: 'PDF_417,QR_CODE'
   };
 
   constructor(
@@ -68,14 +68,14 @@ export class QrService implements OnInit {
             localStorage.setItem('mesa', mesa);
             resolve();
           }else if(mesa.client != client.id){
-            reject("a"); //Mesa incorrecta
+            reject('a'); //Mesa incorrecta
           }else if(!this.pudoIngresarAlLocal){
-            reject("b"); //Sin registar
+            reject('b'); //Sin registar
           }
         }
   
         else{ //qr no existe
-          reject("El código QR escaneado es incorrecto.");
+          reject('El código QR escaneado es incorrecto.');
         }
       });
 
@@ -105,24 +105,61 @@ export class QrService implements OnInit {
   }
 
   GetJsonFromBarcode(data) {
-    var datos = data.split("@");
+    var datos = data.split('@');
     var nombre = datos[2].charAt(0).toUpperCase() + datos[2].slice(1).toLowerCase();
     var apellido = datos[1].charAt(0).toUpperCase() + datos[1].slice(1).toLowerCase();
     var cuil1 = datos[8].substring(0, 2);
     var cuil2 = datos[8].substring(3, 1);
     var cuil = cuil1 + datos[4] + cuil2;
 
-    return { "nombre": nombre, "apellido": apellido, "dni": datos[4], "cuil": cuil };
+    return { 'nombre': nombre, 'apellido': apellido, 'dni': datos[4], 'cuil': cuil };
   }
 
 
   qrPropina(){
+
     return new Promise( (resolve, reject) => {
 
       this.barcodeScanner.scan(this.options)
         .then(barcodeData => {
-          var json = this.GetJsonFromBarcode(barcodeData);
-          resolve(json);
+          const result = barcodeData.text as string;
+          const canceled = barcodeData.cancelled;
+          let jsonTips = {
+            tipo : 'Sin propina',
+            desc: 0,
+          };
+          
+
+          if(result.length > 0){
+
+
+          switch (result) {
+              case 'Excelente':
+                jsonTips.desc = 20;
+                jsonTips.tipo = 'Excelente';
+                break;
+              case 'Muy bien':
+                jsonTips.desc = 15;
+                jsonTips.tipo = 'Muy bien';
+                break;
+              case 'Bien':
+                jsonTips.desc = 10;
+                jsonTips.tipo = 'Bien';
+                break;
+              case 'Regular':
+                jsonTips.desc = 5;
+                jsonTips.tipo = 'Regular';
+                break;
+              case 'Malo':
+                jsonTips.desc = 0;
+                jsonTips.tipo = 'Malo';
+                break;
+              default:
+                break;   
+          }
+
+          resolve(jsonTips);
+        }
         })
         .catch( err => {
           reject(err);
