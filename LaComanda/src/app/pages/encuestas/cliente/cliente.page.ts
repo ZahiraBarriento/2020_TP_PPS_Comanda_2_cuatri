0aa16ec3f35d3ff1e75c36fea2a60574e8580555
LuqiMarino <lucasmarino96@hotmail.com>
2020-11-27 22:13:36 -0300

    asd

--- LaComanda/src/app/pages/encuestas/cliente/cliente.page.ts
+++ LaComanda/src/app/pages/encuestas/cliente/cliente.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { FirestoreService } from '../../../services/firestore.service';
+import { FuctionsService } from 'src/app/services/fuctions.service';
 
 @Component({
   selector: 'app-cliente',
@@ -6,18 +9,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente.page.scss'],
 })
 export class ClientePage implements OnInit {
-
+  image1:string="";
+  image2:string="";
+  image3:string="";
   opinion:string = "";
   protocoloCovid:boolean;
-  constructor() { }
+  yaSubioTodasLasFotos:boolean = false;
+  yaEnvioEncuesta:boolean = false;
+  cantFotos:string="0 / 3";
+
+  constructor(private camera:Camera, private db:FirestoreService, private toastCtrl:FuctionsService) { }
 
   ngOnInit() {
+    this.cantFotos = "0 / 3";
+    this.yaSubioTodasLasFotos = false;
+    this.yaEnvioEncuesta = false;
+  }
+
+  SacarFoto(){
+    let options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 500,
+      targetHeight: 500,
+      quality: 30
+    }
+    this.camera.getPicture(options)
+    .then(imageData => {
+        var imagenAux = `data:image/jpeg;base64,${imageData}`;
+        this.AgregarImagen123(imagenAux);        
+    });
+  }
+
+  AgregarImagen123(img){
+    if (this.cantFotos == "0 / 3" && this.yaSubioTodasLasFotos == false){
+      this.image1 == img;
+      this.cantFotos = "1 / 3";
+      return;
+    }
+    else if (this.cantFotos == "1 / 3" && this.yaSubioTodasLasFotos == false){
+      this.image2 == img;
+      this.cantFotos = "2 / 3";
+      return;
+    }
+    else{
+      this.image3 == img;
+      this.cantFotos = "3 / 3";
+      this.yaSubioTodasLasFotos = true;
+      return;
+    }
   }
 
   enviarEncuesta(){
     var rangoSatisfecho = (<HTMLIonRangeElement>document.getElementById("rango")).value;
     var protocoloCovid = (<HTMLIonRadioGroupElement>document.getElementById("grupo")).value == "true";
-    console.log(protocoloCovid);
+    var selectString = (<HTMLIonSelectElement>document.getElementById("select")).value;
+    var mesaConSal = (<HTMLIonCheckboxElement>document.getElementById("chkSal")).checked;
+    var mesaConEscarbadientes = (<HTMLIonCheckboxElement>document.getElementById("chkEscarbadientes")).checked;
+    var mesaConServilletas = (<HTMLIonCheckboxElement>document.getElementById("chkServilletas")).checked;
+    var mesaConAderezos = (<HTMLIonCheckboxElement>document.getElementById("chkAderezos")).checked;
+    var clienteNombre = localStorage.getItem('userCatch')["nombre"];
+
+    var json = {
+      "cliente": clienteNombre,
+      "rangoSatisfecho": rangoSatisfecho,
+      "protocoloCovid": protocoloCovid,
+      "select": selectString,
+      "mesaConSal": mesaConSal,
+      "mesaConEscarvadientes": mesaConEscarbadientes,
+      "mesaConServilletas": mesaConServilletas,
+      "mesaConAderezos": mesaConAderezos
+    }
+    this.db.addData('encuestasCliente', json);
+    this.toastCtrl.presentToast("Encuesta enviada con exito, Muchas gracias por tu opinion!!", 'success');
+    setTimeout(() => {
+      this.yaEnvioEncuesta = true;
+    }, 2000);
   }
 
 }
